fe7c6da55c387da5e176486ec4072a3bd1032b3c
Koustav-iitropar <2015csb1017@iitrpr.ac.in>
2017-03-30 15:30:51 +0530

    Complaint_dialogue box

---
+++ Complaint_Lodging.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+
+<style>
+#titl
+{
+position: absolute;
+left: 400px;
+top: 10px;
+}
+</style>
+        <title>Lodging a Complaint</title>
+
+</head>
+
+<body>
+<a href="https://www.facebook.com/"> <img src="iitropar.png" width="100" height="100" alt="Hostel"/></a>
+        <h1 id="titl">IIT ROPAR COMPLAINT FORUM</h1>
+
+<form>
+<br><br><br>
+<b>Subject:   </b><input rows="1" cols="100" maxlength="100"> </input>
+</form>
+<br>
+
+<b>Description:   </b><br/><br /><textarea rows="4" cols="150" maxlength="1000">
+</textarea>
+<br>
+<form action="">
+<input type="checkbox" name="Anonymous" value="Yes">Be Anonymous<br>
+<!-- Anonymous variable must be initialized to NO. If the above checkbox is not clicked then the anonymous variable would continue to have "NO". Same concept for "Public". -->
+<input type="checkbox" name="Public" value="Yes">Make Your complaint visible to all<br>
+</form>
+
+ <button type="button">Submit</button>
+
+</body>
+</html>
